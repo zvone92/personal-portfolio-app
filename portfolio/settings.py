4678faa6b8325b4cb0f15fc1fee6e648c161238b
Jhone <vlahzvonimir92@gmail.com>
2020-08-05 21:46:38 +0200

    Prepared for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^$-v#q^ergafq9d#6aa(v137#&9yko105p*7+5*&qgj02a!%m#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://zvlah.com']
 
 
 # Application definition
@@ -74,18 +74,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myportfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': '16162252019',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
-}
-
+DATABASES = os.environ['DATABASES']
 
 
 # Password validation
@@ -134,8 +123,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-
-application = get_wsgi_application()
+from dj_static import Cling
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ََAPPNAME.settings')
+application = Cling(get_wsgi_application())
